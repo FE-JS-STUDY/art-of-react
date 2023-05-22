e2a8a632514009617ed867693c8d523affab576c
김윤하 <beupthere@gmail.com>
2023-05-22 21:51:11 +0900

    11 컴포넌트 성능 최적화 11.7

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
@@ -16,4 +17,4 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList); //리스트 관련된 컴포넌트 최적화 시 리스트 아이템과 리스트 두 가지 모두 최적화하기.
