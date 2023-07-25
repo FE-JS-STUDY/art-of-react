55f73d1743e3aec4b25951d9029cd8ae5065dec5
yulloe <beupthere@gmail.com>
2023-07-25 14:21:13 +0900

    18 리덕스 미들웨어를 통한 비동기 작업 관리 18.3.2.2~3

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,20 +1,65 @@
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from "redux-saga/effects";
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+import { startLoading, finishLoading } from "./loading";
 
 // 액션 타입 선언
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
-// thunk 함수 생성
-// thunk 함수 내부에서는 시작했을 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 함
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST)); // 로딩시작
+  // 파라미터로 action 받아오면 액션 정보 조회 가능
+  try {
+    // call 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있음
+    // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
+    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload) 의미
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    // try/catch 문 써서 에러 잡을 수 있음
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST)); // 로딩 완료
+}
+
+function* getUsersSaga(action) {
+  yield put(startLoading(GET_USERS));
+  try {
+    const users = yield call(api.getUsers);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // 초기 상태를 선언
 // 요청의 로딩 중 상태는 loading이라는 객체에서 관리
@@ -27,18 +72,10 @@ const sample = handleActions(
   {
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
       post: action.payload,
     }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false,
-      },
       users: action.payload,
     }),
   },
